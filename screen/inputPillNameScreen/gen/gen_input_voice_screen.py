85be898011a3eb90c63f6abe49987ce82c882ef7
paedayz <pae16phasit@gmail.com>
2021-11-16 19:25:07 +0700

    :wrench: delete gen comment and class store :wrench:

--- screen/inputPillNameScreen/gen/gen_input_voice_screen.py
+++ screen/inputPillNameScreen/gen/gen_input_voice_screen.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'input_voice_screen.ui'
-#
-# Created by: PyQt5 UI code generator 5.10
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_input_voice(object):
--- screen/pillSummaryScreen/gen/gen_pill_summary_screen.py
+++ screen/pillSummaryScreen/gen/gen_pill_summary_screen.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'pill_summary_screen.ui'
-#
-# Created by: PyQt5 UI code generator 5.10
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_pill_summary_screen(object):
